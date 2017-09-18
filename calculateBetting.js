ffd46136b4d4a9d67c98ee489b0818cf72a546f1
Vinay Kumar Singh <vsi120@sapient.com>
2017-09-18 10:47:54 +0530

    put check for data records fro calculaton

--- calculateBetting.js
+++ calculateBetting.js
@@ -5,15 +5,17 @@ var _ = require('lodash');
 module.exports.winbet = function(windata,result){
 	var totalWinTicket=0,totalPoolValue=0, windividend=0, resultArr = [];
 	resultArr = _.split(result,':');
-	_.forEach(windata,function(value,key){
-		var windataArr= [];
-		windataArr = _.split(value,':');
-		totalPoolValue=totalPoolValue+_.parseInt(windataArr[3]);		
-		if(windataArr[2] === resultArr[1]){
-			totalWinTicket=totalWinTicket+_.parseInt(windataArr[3]);
-		}
-
-	});
+	if(windata.length >0){
+		_.forEach(windata,function(value,key){
+			var windataArr= [];
+			windataArr = _.split(value,':');
+			totalPoolValue=totalPoolValue+_.parseInt(windataArr[3]);		
+			if(windataArr[2] === resultArr[1]){
+				totalWinTicket=totalWinTicket+_.parseInt(windataArr[3]);
+			}
+		});	
+	}
+	
 	if(totalWinTicket>0)
 	{
 		windividend =  (totalPoolValue*process.env.WIN_COMMISSION)/100;
@@ -32,26 +34,26 @@ module.exports.placebet = function(placedata,result){
 	//console.log("--placebet calculation-----");
 	var totalPlaceTicket=0,totalPoolValue=0, placedividend=0, resultArr = [], placeresult=[];
 	resultArr = _.split(result,':');
-	_.forEach(placedata,function(value,key){
-		var placedataArr= [];
-		placedataArr = _.split(value,':');
-		totalPoolValue=totalPoolValue+_.parseInt(placedataArr[3]);
-		if(_.indexOf(resultArr, placedataArr[2]) >0)
-		{
-			if(typeof placeresult[placedataArr[2]] !=='undefined')
+	if(placedata.length >0){
+		_.forEach(placedata,function(value,key){
+			var placedataArr= [];
+			placedataArr = _.split(value,':');
+			totalPoolValue=totalPoolValue+_.parseInt(placedataArr[3]);
+			if(_.indexOf(resultArr, placedataArr[2]) >0)
 			{
+				if(typeof placeresult[placedataArr[2]] !=='undefined')
+				{
 
-				placeresult[placedataArr[2]] = placeresult[placedataArr[2]] + _.parseInt(placedataArr[3]) ; 
-			}else{
-				placeresult[placedataArr[2]]= 0;
-				placeresult[placedataArr[2]]= placeresult[placedataArr[2]] + _.parseInt(placedataArr[3]) ; 
+					placeresult[placedataArr[2]] = placeresult[placedataArr[2]] + _.parseInt(placedataArr[3]) ; 
+				}else{
+					placeresult[placedataArr[2]]= 0;
+					placeresult[placedataArr[2]]= placeresult[placedataArr[2]] + _.parseInt(placedataArr[3]) ; 
 
+				}				
 			}
-			
-		}     		
-
-	});
-	
+		});
+	}
+		
 	var placeBetting='';
 	if(placeresult.length >0)
 	{
@@ -78,16 +80,18 @@ module.exports.exactabet = function(exactadata,result){
 	var totalexactTicket=0, exactadividend=0,totalPoolValue=0, resultArr = [], exactaresult=[];
 	resultArr = _.split(result,':');
 	var compareVaule = resultArr[1]+','+resultArr[2];
-	_.forEach(exactadata,function(value,key){
-		var exactadataArr= [];
-		exactadataArr = _.split(value,':');	
-		totalPoolValue=totalPoolValue+_.parseInt(exactadataArr[3]);	
-		if(exactadataArr[2] === compareVaule)
-		{
-			totalexactTicket= totalexactTicket + _.parseInt(exactadataArr[3]);			
-		}     		
-
-	});
+	if(exactadata.length >0){
+		_.forEach(exactadata,function(value,key){
+			var exactadataArr= [];
+			exactadataArr = _.split(value,':');	
+			totalPoolValue=totalPoolValue+_.parseInt(exactadataArr[3]);	
+			if(exactadataArr[2] === compareVaule)
+			{
+				totalexactTicket= totalexactTicket + _.parseInt(exactadataArr[3]);			
+			}
+		});	
+	}
+	
 	var exactearetting='';
 	if(totalexactTicket > 0){
 
